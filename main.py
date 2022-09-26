70adebea03bbba057d63b6892a88f6938b55ef6c
alanoudalshalani <107769859+alanoudalshalani@users.noreply.github.com>
2022-09-26 19:18:27 +0300

    done with the challenge

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-# Create a dictionary called `person`, it has three properties, `name` which is a string, `age` which is an integer, `hobbies` which is a list of strings.
+# Create a dictionary called `person`, it has three properties, `name` which is a string,
+# `age` which is an integer, `hobbies` which is a list of strings.
+
+person = {
+    "name" : "Alanoud",
+    "age" : 20 ,
+    "hobbies" : ["reading", "writting"]
+}
 
 
-# Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
+# Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, 
+#this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
 
 
 def change_age(person, number):
-    ...
-
+    person["age"] = number
+    return person
 
 print(change_age(person, 21))
 
-
-# Create a function called `add_hobby` which takes the `person` dictionary and a `hobby` as arguments. This function adds the hobby to the list of `hobbies` inside the person dictionary. The function will return the updated dictionary.
+# Create a function called `add_hobby` which takes the `person` dictionary and a `hobby` as arguments. This function adds the 
+#hobby to the list of `hobbies` inside the person dictionary. The function will return the updated dictionary.
 
 
 def add_hobby(person, hobby):
-    ...
+    results = []
+    person["hobbies"].append(hobby)
+    return person
 
 
-print(add_hobby(person, "Table Tennis"))
+print(add_hobby(person, "swimming"))
 
 
 # CHALLENGE
 
-# Create a function called `add_job` which takes `person` dictionary and `job` which is a string, as arguments. This function checks, if the dictionary does not have a property called `job` then it will add it to the dictionary. The function will return the updated dictionary.
+# Create a function called `add_job` which takes `person` dictionary and `job` which is a string, as arguments. 
+#This function checks, if the dictionary does not have a property called `job` then it will add it to the dictionary. 
+#The function will return the updated dictionary.
 
 
-def add_job(person, job):
-    ...
+# def add_job(person, job):
+#     for job , value in job:
+#         if person == value
+#         return person
+#         else person['job']=engineer 
+#         print(person)
 
 
-# 2. Create a function called `check_hobbies`, which takes a person dictionary as an argument. This function checks the number of hobbies for this person, if it's more than three hobbies, print to the user a message telling them that they're talented.
+# 2. Create a function called `check_hobbies`, which takes a person dictionary as an argument. 
+#This function checks the number of hobbies for this person, if it's more than three hobbies, 
+#print to the user a message telling them that they're talented.
 
 
-def check_hobbies(person):
-    ...
+# def check_hobbies(person):
+#     for hobbies in person:
+#         if len(hobbies)>3
+#         print("you're talented")
+
